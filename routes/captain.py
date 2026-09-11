10e93e95f95872cfde0e670630dca06f76d23669
Ishit Dandawate <ishit1561@gmail.com>
2026-09-11 14:50:47 +0530

    Trigger delay sync after PDF upload

--- routes/captain.py
+++ routes/captain.py
@@ -851,6 +851,10 @@ def complete_survey():
 
             db.session.commit()
 
+            from utils.auto_sync import start_defect_delay_sync_if_ready
+
+            start_defect_delay_sync_if_ready(survey)
+
             # -----------------------------------
             # CLEAR ONLY CURRENT SURVEY
             # -----------------------------------
@@ -1623,6 +1627,10 @@ def reupload_survey_pdf(survey_id):
         survey.pdf_reupload_reason = None
 
         db.session.commit()
+
+        from utils.auto_sync import start_defect_delay_sync_if_ready
+
+        start_defect_delay_sync_if_ready(survey)
 
         # -----------------------------------
         # SUCCESS
